9bf95f960691afa84be643d866d1fb1995e60fd9
Hao-Wen Dong <salu.hwdong@gmail.com>
2020-04-03 12:31:50 -0700

    Import functions

--- muspy/representations/__init__.py
+++ muspy/representations/__init__.py
@@ -1,1 +1,11 @@
 """Representation utilities."""
+
+from .event import music_to_event_representation
+from .note import music_to_note_representation
+from .pianoroll import music_to_pianoroll_representation
+
+__all__ = [
+    "music_to_event_representation",
+    "music_to_note_representation",
+    "music_to_pianoroll_representation",
+]
