28ecf086a65fa46a821ca4f0f452119a4e12163b
Hao-Wen Dong <salu.hwdong@gmail.com>
2020-04-03 22:18:32 -0700

    Import functions from submodules

--- muspy/io/__init__.py
+++ muspy/io/__init__.py
@@ -1,1 +1,14 @@
 """I/O utilities."""
+from .midi import parse_pretty_midi, read_midi, to_pretty_midi, write_midi
+from .musicxml import read_musicxml, write_musicxml
+from .pianoroll import to_pypianoroll
+
+__all__ = [
+    "parse_pretty_midi",
+    "read_midi",
+    "read_musicxml",
+    "to_pretty_midi",
+    "to_pypianoroll",
+    "write_midi",
+    "write_musicxml",
+]
