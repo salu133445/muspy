bb611941478543534c5fc4564b189b273d2fb158
Hao-Wen Dong <salu.hwdong@gmail.com>
2020-04-14 17:09:49 -0700

    Use wildcard import for simplicity

--- muspy/__init__.py
+++ muspy/__init__.py
@@ -4,20 +4,24 @@ MusPy
 
 A Python package for processing symbolic music and working with common music
 datasets.
+
 """
 from . import (
+    classes,
     core,
     datasets,
     inputs,
     metrics,
+    music,
     outputs,
     representations,
     schemas,
     visualization,
 )
+from .classes import *  # noqa: F401,F403
 from .core import *  # noqa: F401,F403
 from .inputs import *  # noqa: F401,F403
-from .music import Music
+from .music import *  # noqa: F401,F403
 from .outputs import *  # noqa: F401,F403
 from .representations import *  # noqa: F401,F403
 from .schemas import *  # noqa: F401,F403
@@ -26,18 +30,18 @@ from .version import __version__
 __all__ = [
     "__version__",
     "datasets",
+    "inputs",
     "metrics",
-    "quantize",
-    "quantize_absolute_timing",
-    "quantize_by_beats",
+    "outputs",
     "representations",
-    "sort",
-    "to_ordered_dict",
-    "transpose",
+    "schemas",
     "visualization",
+    "classes",
 ]
+__all__.extend(classes.__all__)
 __all__.extend(core.__all__)
 __all__.extend(inputs.__all__)
+__all__.extend(music.__all__)
 __all__.extend(outputs.__all__)
 __all__.extend(representations.__all__)
 __all__.extend(schemas.__all__)
