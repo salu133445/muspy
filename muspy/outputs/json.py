020c620816f5aec6997b5db2e9f428834d986d50
Hao-Wen Dong <salu.hwdong@gmail.com>
2020-12-24 10:08:08 -0800

    Add `ensure_ascii` & `indent` kwargs to save_json

--- muspy/outputs/json.py
+++ muspy/outputs/json.py
@@ -1,13 +1,18 @@
 """JSON output interface."""
 import json
 from pathlib import Path
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Optional, Union
 
 if TYPE_CHECKING:
     from ..music import Music
 
 
-def save_json(path: Union[str, Path], music: "Music"):
+def save_json(
+    path: Union[str, Path],
+    music: "Music",
+    ensure_ascii: bool = False,
+    indent: Optional[Union[int, str]] = None,
+):
     """Save a Music object to a JSON file.
 
     Parameters
@@ -16,7 +21,15 @@ def save_json(path: Union[str, Path], music: "Music"):
         Path to save the JSON file.
     music : :class:`muspy.Music`
         Music object to save.
+    ensure_ascii : bool
+        Whether to escape non-ASCII characters. Defaults to False. See
+        :py:func:`json.dumps`.
+    indent : int or str
+        Indent level. See :py:func:`json.dumps`.
 
     """
     with open(str(path), "w") as f:
-        f.write(json.dumps(music.to_ordered_dict()))
+        json_str = json.dumps(
+            music.to_ordered_dict(), ensure_ascii=ensure_ascii, indent=indent
+        )
+        f.write(json_str)
