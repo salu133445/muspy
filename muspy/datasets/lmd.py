497fb2958c2fa67eae5897bc028111b51a2e743a
Hao-Wen Dong <salu.hwdong@gmail.com>
2021-08-11 01:26:41 -0700

    Fix LMD sizes

--- muspy/datasets/lmd.py
+++ muspy/datasets/lmd.py
@@ -34,7 +34,7 @@ class LakhMIDIDataset(RemoteFolderDataset):
             "filename": "lmd_full.tar.gz",
             "url": "http://hog.ee.columbia.edu/craffel/lmd/lmd_full.tar.gz",
             "archive": True,
-            "size": 35727800,
+            "size": 1768163879,
             "md5": "2536ce3fd2cede53ddaa264f731859ab",
             "sha256": "6fcfe2ac49ca08f3f214cec86ab138d4fc4dabcd7f27f491a838dae6db45a12b",
         }
@@ -56,6 +56,7 @@ class LakhMIDIMatchedDataset(RemoteFolderDataset):
             "filename": "lmd_matched.tar.gz",
             "url": "http://hog.ee.columbia.edu/craffel/lmd/lmd_matched.tar.gz",
             "archive": True,
+            "size": 1407072670,
             "md5": "fb80d01c22020295bb3eeef31f1aa63a",
             "sha256": "621ff830aed771f469e5bfa13dc12a33c6ed69090adeda63d0b5c47783af0191",
         }
@@ -77,7 +78,7 @@ class LakhMIDIAlignedDataset(RemoteFolderDataset):
             "filename": "lmd_aligned.tar.gz",
             "url": "http://hog.ee.columbia.edu/craffel/lmd/lmd_aligned.tar.gz",
             "archive": True,
-            "size": 1407072670,
+            "size": 272169548,
             "md5": "d36ca9159966d81d97e1e37d10ed4584",
             "sha256": "2bf5400e82eba73204644946515489b68811e1e656b0cfd854efc14377f6e53b",
         }
