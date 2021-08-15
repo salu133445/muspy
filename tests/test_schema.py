8a68c8610ff2130aaf408c3a85111fe37beb47c6
Hao-Wen Dong <salu.hwdong@gmail.com>
2021-08-15 01:47:33 -0700

    Update MusicXML schema to v4.0

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,7 +1,7 @@
 """Test cases for schema validations."""
 import muspy
 
-from .utils import TEST_JSON_PATH, TEST_YAML_PATH, TEST_MUSICXML_LILYPOND_DIR
+from .utils import TEST_JSON_PATH, TEST_MUSICXML_DIR, TEST_YAML_PATH
 
 
 def test_yaml_schema():
@@ -13,6 +13,4 @@ def test_json_schema():
 
 
 def test_musicxml_schema():
-    muspy.validate_musicxml(
-        TEST_MUSICXML_LILYPOND_DIR / "01a-Pitches-Pitches.xml"
-    )
+    muspy.validate_musicxml(TEST_MUSICXML_DIR / "fur-elise.xml")
