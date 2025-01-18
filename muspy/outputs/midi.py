de7cb1d4743eb4baa61cc71ea5722557f68c0f0a
pnlong <phillipnlong@gmail.com>
2025-01-17 17:36:52 -0800

    fixed bug in midi output

--- muspy/outputs/midi.py
+++ muspy/outputs/midi.py
@@ -248,7 +248,10 @@ def to_mido_meta_track(music: "Music", realize_annotations: bool = False) -> Mid
     # create a track to store the metadata
     meta_track = MidiTrack()
     all_notes = sum((track.notes for track in music.tracks), []) # all notes
-    max_note_time = max((note.time for note in all_notes))
+    if len(all_notes) == 0:
+        return meta_track
+    else:
+        max_note_time = max((note.time for note in all_notes))
 
     # song title
     if music.metadata.title is not None:
